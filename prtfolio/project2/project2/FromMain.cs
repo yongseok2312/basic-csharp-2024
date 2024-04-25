756c88d3485131a86db5586a50136fc32f9c7777
yongseok2312 <lyos2312@naver.com>
2024-04-25 16:37:57 +0900

    portfolio

---
+++ prtfolio/project2/project2/FromMain.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace project2
+{
+
+    public partial class FromMain : MetroForm
+    {
+
+        public FromMain()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly string connString = "Data Source=localhost;" +
+                                           "Initial Catalog=menu;" +
+                                           "Persist Security Info=True;" +
+                                           "User ID=sa;Encrypt=False;" +
+                                           "Password=mssql_p@ss";
+        private void FromMain_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"SELECT [메뉴 번호]
+                                          ,[메뉴이름]
+                                          ,[종류]
+                                          ,[밥,면]
+                                          ,[가격]
+                                          FROM menu";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "menu");
+
+                    DgvResult.DataSource = ds.Tables["menu"];
+                    DgvResult.ReadOnly = true;
+                    DgvResult.Columns[0].Visible = false;
+                    DgvResult.Columns[1].HeaderText = "메뉴 이름";
+                    DgvResult.Columns[2].HeaderText = "종류";
+                    DgvResult.Columns[3].Visible = false;
+                    DgvResult.Columns[4].HeaderText = "가격";
+
+                    DgvResult.Columns[1].Width = 90;
+                    DgvResult.Columns[2].Width = 55;
+                    DgvResult.Columns[4].Width = 90;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"데이터 가져오기 오류: {ex.Message}");
+                }
+
+            }
+        }
+        #region "종류"
+
+        private void ChkKorean_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+            
+        }
+        private void ChkChina_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+        }
+
+        private void ChkWestern_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+        }
+        private void ChkJapan_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+        }
+
+        #endregion
+
+        private void modify()
+        {
+            bool showrice = Rbtnrice.Checked;
+            bool showNuddle = RbtnNuddle.Checked;
+
+            if(DgvResult.DataSource is DataTable dt)
+            {
+                DataView dv = dt.DefaultView;
+                string filterExpression = "";
+                if (showrice)
+                {
+                    filterExpression = "[밥,면] = '밥'";
+                }
+                else if (showNuddle)
+                {
+                    filterExpression = "[밥,면] = '면'";
+                }
+                else
+                {
+                    filterExpression = "";
+                }
+                dv.RowFilter = filterExpression;
+                DgvResult.DataSource = dv.ToTable();
+            }
+        } // 밥, 면 설정 함수
+
+        #region "밥,면"
+        private void Rbtnrice_Click(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();   
+        }
+
+        private void RbtnNuddle_Click(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+            
+        }
+
+        private void Rbtnnomal_Click(object sender, EventArgs e)
+        {
+            FilterMenu();
+            modify();
+        }
+        #endregion
+        private void FilterMenu()
+        {
+            bool showKorean = ChkKorean.Checked;
+            bool showJapanese = ChkJapan.Checked;
+            bool showChina = ChkChina.Checked;
+            bool showWestern = ChkWestern.Checked;
+
+            try
+            {
+                
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT [메뉴 번호], 메뉴이름, 종류, [밥,면], 가격 FROM menu";
+                    SqlCommand command = new SqlCommand(query, conn);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "menu");
+
+                    DataView dv = ds.Tables["menu"].DefaultView;
+                    string filterExpression = "";
+
+
+                    if (showKorean && showJapanese && showChina && showWestern)
+                    {
+                        filterExpression = "종류 IN ('한식','일식','중식','양식')";
+                    }
+                    else if (showKorean && showJapanese && showChina)
+                    {
+                        filterExpression = "종류 IN ('한식','일식','중식')";
+                    }
+                    else if (showKorean && showJapanese && showWestern)
+                    {
+                        filterExpression = "종류 IN ('한식','일식','양식')";
+                    }
+                    else if (showJapanese && showChina && showWestern)
+                    {
+                        filterExpression = "종류 IN ('일식','중식','양식')";
+                    }
+                    else if (showKorean && showJapanese)
+                    {
+                        filterExpression = "종류 IN ('한식', '일식')";
+                    }
+                    else if (showJapanese && showChina)
+                    {
+                        filterExpression = "종류 IN ('일식','중식')";
+                    }
+                    else if (showChina && showWestern)
+                    {
+                        filterExpression = "종류 IN ('중식','양식')";
+                    }
+                    else if (showJapanese && showWestern)
+                    {
+                        filterExpression = "종류 IN('일식','양식')";
+
+                    }
+                    else if (showKorean && showChina)
+                    {
+                        filterExpression = "종류 IN('한식','중식')";
+
+                    }
+                    else if (showKorean && showWestern)
+                    {
+                        filterExpression = "종류 IN ('한식','양식')";
+                    }
+                    else if (showKorean)
+                    {
+                        filterExpression = "종류 = '한식'";
+                    }
+                    else if (showJapanese)
+                    {
+                        filterExpression = "종류 = '일식'";
+                    }
+                    else if (showChina)
+                    {
+                        filterExpression = "종류 = '중식'";
+                    }
+                    else if (showWestern)
+                    {
+                        filterExpression = "종류 = '양식'";
+                    }
+                    dv.RowFilter = filterExpression;
+
+                    DgvResult.DataSource = dv.ToTable();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터베이스 연동 오류: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void price(int maxPrice)
+        {
+            try
+            {
+                if (DgvResult.DataSource is DataTable dt)
+                {
+                    DataView dv = dt.DefaultView;
+                    string filterExpression = $"가격 <= {maxPrice}";
+                    dv.RowFilter = filterExpression;
+                    DataTable filteredTable = dv.ToTable();
+  
+                }
+                else
+                {
+                    MessageBox.Show("유효한 데이터 소스가 아닙니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류: {ex.Message}", "오류 발생", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Btnstart_Click(object sender, EventArgs e)
+        {
+            if (DgvResult != null && DgvResult.Rows.Count > 0 && DgvResult.DataSource is DataTable dt)
+            {
+                // DataTable에서 랜덤하게 하나의 행(메뉴) 선택
+                Random random = new Random();
+                int randomIndex = random.Next(0, dt.Rows.Count); // 0부터 메뉴 개수 - 1까지 랜덤 인덱스 생성
+
+                DataRow randomMenu = dt.Rows[randomIndex];
+                string menuName = randomMenu["메뉴이름"].ToString();
+                string menuCategory = randomMenu["종류"].ToString();
+                decimal menuPrice = Convert.ToDecimal(randomMenu["가격"]);
+
+                metroTextBox1.Text = ("오늘의 음식은: "+menuName +Environment.NewLine+"예상 가격은 :" + menuPrice+"원");
+               
+
+
+            }
+            else
+            {
+                MessageBox.Show("메뉴 데이터가 없습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        } // random
+
+        private void PriceBar_Scroll(object sender, EventArgs e)
+        {
+            modify();
+            FilterMenu();
+            int p = PriceBar.Value;
+            TxtPrice.Text = p.ToString();
+            price(p);
+        }
+    } }
+            
+        
+    
+
+
+        
---
+++ prtfolio/최종본/project2/FromMain.Designer.cs
@@ -0,0 +1,289 @@
+﻿using Microsoft.Identity.Client;
+
+namespace project2
+{
+    partial class FromMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChkWestern = new MetroFramework.Controls.MetroCheckBox();
+            this.ChkChina = new MetroFramework.Controls.MetroCheckBox();
+            this.ChkJapan = new MetroFramework.Controls.MetroCheckBox();
+            this.ChkKorean = new MetroFramework.Controls.MetroCheckBox();
+            this.DgvResult = new System.Windows.Forms.DataGridView();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.Btnstart = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TxtPrice = new MetroFramework.Controls.MetroTextBox();
+            this.PriceBar = new System.Windows.Forms.TrackBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Rbtnnomal = new MetroFramework.Controls.MetroRadioButton();
+            this.RbtnNuddle = new MetroFramework.Controls.MetroRadioButton();
+            this.Rbtnrice = new MetroFramework.Controls.MetroRadioButton();
+            this.metroTextBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvResult)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PriceBar)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ChkWestern);
+            this.groupBox1.Controls.Add(this.ChkChina);
+            this.groupBox1.Controls.Add(this.ChkJapan);
+            this.groupBox1.Controls.Add(this.ChkKorean);
+            this.groupBox1.Location = new System.Drawing.Point(339, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 76);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "종류";
+            // 
+            // ChkWestern
+            // 
+            this.ChkWestern.AutoSize = true;
+            this.ChkWestern.Location = new System.Drawing.Point(84, 46);
+            this.ChkWestern.Name = "ChkWestern";
+            this.ChkWestern.Size = new System.Drawing.Size(47, 15);
+            this.ChkWestern.TabIndex = 4;
+            this.ChkWestern.TabStop = false;
+            this.ChkWestern.Text = "양식";
+            this.ChkWestern.UseVisualStyleBackColor = true;
+            this.ChkWestern.CheckedChanged += new System.EventHandler(this.ChkWestern_CheckedChanged);
+            // 
+            // ChkChina
+            // 
+            this.ChkChina.AutoSize = true;
+            this.ChkChina.Location = new System.Drawing.Point(22, 46);
+            this.ChkChina.Name = "ChkChina";
+            this.ChkChina.Size = new System.Drawing.Size(47, 15);
+            this.ChkChina.TabIndex = 3;
+            this.ChkChina.TabStop = false;
+            this.ChkChina.Text = "중식";
+            this.ChkChina.UseVisualStyleBackColor = true;
+            this.ChkChina.CheckedChanged += new System.EventHandler(this.ChkChina_CheckedChanged);
+            // 
+            // ChkJapan
+            // 
+            this.ChkJapan.AutoSize = true;
+            this.ChkJapan.Location = new System.Drawing.Point(84, 25);
+            this.ChkJapan.Name = "ChkJapan";
+            this.ChkJapan.Size = new System.Drawing.Size(47, 15);
+            this.ChkJapan.TabIndex = 2;
+            this.ChkJapan.TabStop = false;
+            this.ChkJapan.Text = "일식";
+            this.ChkJapan.UseVisualStyleBackColor = true;
+            this.ChkJapan.CheckedChanged += new System.EventHandler(this.ChkJapan_CheckedChanged);
+            // 
+            // ChkKorean
+            // 
+            this.ChkKorean.AutoSize = true;
+            this.ChkKorean.Location = new System.Drawing.Point(22, 25);
+            this.ChkKorean.Name = "ChkKorean";
+            this.ChkKorean.Size = new System.Drawing.Size(47, 15);
+            this.ChkKorean.TabIndex = 1;
+            this.ChkKorean.TabStop = false;
+            this.ChkKorean.Text = "한식";
+            this.ChkKorean.UseVisualStyleBackColor = true;
+            this.ChkKorean.CheckedChanged += new System.EventHandler(this.ChkKorean_CheckedChanged);
+            // 
+            // DgvResult
+            // 
+            this.DgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvResult.Location = new System.Drawing.Point(23, 83);
+            this.DgvResult.Name = "DgvResult";
+            this.DgvResult.ReadOnly = true;
+            this.DgvResult.RowTemplate.Height = 23;
+            this.DgvResult.Size = new System.Drawing.Size(296, 214);
+            this.DgvResult.TabIndex = 3;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel1.TabIndex = 7;
+            this.metroLabel1.Text = "오늘의 메뉴";
+            // 
+            // Btnstart
+            // 
+            this.Btnstart.Location = new System.Drawing.Point(340, 245);
+            this.Btnstart.Name = "Btnstart";
+            this.Btnstart.Size = new System.Drawing.Size(73, 52);
+            this.Btnstart.TabIndex = 2;
+            this.Btnstart.Text = "START!!";
+            this.Btnstart.UseVisualStyleBackColor = true;
+            this.Btnstart.Click += new System.EventHandler(this.Btnstart_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TxtPrice);
+            this.groupBox2.Controls.Add(this.PriceBar);
+            this.groupBox2.Location = new System.Drawing.Point(340, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 81);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "가격";
+            // 
+            // TxtPrice
+            // 
+            this.TxtPrice.Location = new System.Drawing.Point(163, 52);
+            this.TxtPrice.Name = "TxtPrice";
+            this.TxtPrice.PromptText = "가격";
+            this.TxtPrice.ReadOnly = true;
+            this.TxtPrice.Size = new System.Drawing.Size(75, 23);
+            this.TxtPrice.TabIndex = 0;
+            this.TxtPrice.TabStop = false;
+            this.TxtPrice.Text = "가격";
+            // 
+            // PriceBar
+            // 
+            this.PriceBar.Location = new System.Drawing.Point(8, 18);
+            this.PriceBar.Maximum = 20000;
+            this.PriceBar.Minimum = 4000;
+            this.PriceBar.Name = "PriceBar";
+            this.PriceBar.Size = new System.Drawing.Size(230, 45);
+            this.PriceBar.SmallChange = 1000;
+            this.PriceBar.TabIndex = 1;
+            this.PriceBar.TabStop = false;
+            this.PriceBar.Tag = "";
+            this.PriceBar.TickFrequency = 1000;
+            this.PriceBar.Value = 15000;
+            this.PriceBar.Scroll += new System.EventHandler(this.PriceBar_Scroll);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Rbtnnomal);
+            this.groupBox3.Controls.Add(this.RbtnNuddle);
+            this.groupBox3.Controls.Add(this.Rbtnrice);
+            this.groupBox3.Location = new System.Drawing.Point(497, 63);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(87, 87);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "밥,면";
+            // 
+            // Rbtnnomal
+            // 
+            this.Rbtnnomal.AutoSize = true;
+            this.Rbtnnomal.Location = new System.Drawing.Point(12, 61);
+            this.Rbtnnomal.Name = "Rbtnnomal";
+            this.Rbtnnomal.Size = new System.Drawing.Size(71, 15);
+            this.Rbtnnomal.TabIndex = 7;
+            this.Rbtnnomal.TabStop = true;
+            this.Rbtnnomal.Text = "상관없음";
+            this.Rbtnnomal.UseVisualStyleBackColor = true;
+            this.Rbtnnomal.Click += new System.EventHandler(this.Rbtnnomal_Click);
+            // 
+            // RbtnNuddle
+            // 
+            this.RbtnNuddle.AutoSize = true;
+            this.RbtnNuddle.Location = new System.Drawing.Point(12, 41);
+            this.RbtnNuddle.Name = "RbtnNuddle";
+            this.RbtnNuddle.Size = new System.Drawing.Size(35, 15);
+            this.RbtnNuddle.TabIndex = 6;
+            this.RbtnNuddle.TabStop = true;
+            this.RbtnNuddle.Text = "면";
+            this.RbtnNuddle.UseVisualStyleBackColor = true;
+            this.RbtnNuddle.Click += new System.EventHandler(this.RbtnNuddle_Click);
+            // 
+            // Rbtnrice
+            // 
+            this.Rbtnrice.AutoSize = true;
+            this.Rbtnrice.Location = new System.Drawing.Point(12, 20);
+            this.Rbtnrice.Name = "Rbtnrice";
+            this.Rbtnrice.Size = new System.Drawing.Size(35, 15);
+            this.Rbtnrice.TabIndex = 5;
+            this.Rbtnrice.TabStop = true;
+            this.Rbtnrice.Text = "밥";
+            this.Rbtnrice.UseVisualStyleBackColor = true;
+            this.Rbtnrice.Click += new System.EventHandler(this.Rbtnrice_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(430, 245);
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.ReadOnly = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(154, 50);
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.TabStop = false;
+            this.metroTextBox1.Text = "과연";
+            // 
+            // FromMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 308);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Btnstart);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.DgvResult);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FromMain";
+            this.Text = "메뉴 정하기";
+            this.Load += new System.EventHandler(this.FromMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvResult)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PriceBar)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView DgvResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Button Btnstart;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar PriceBar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private MetroFramework.Controls.MetroCheckBox ChkJapan;
+        private MetroFramework.Controls.MetroCheckBox ChkKorean;
+        private MetroFramework.Controls.MetroCheckBox ChkWestern;
+        private MetroFramework.Controls.MetroCheckBox ChkChina;
+        private MetroFramework.Controls.MetroRadioButton RbtnNuddle;
+        private MetroFramework.Controls.MetroRadioButton Rbtnrice;
+        private MetroFramework.Controls.MetroRadioButton Rbtnnomal;
+        private MetroFramework.Controls.MetroTextBox TxtPrice;
+        private System.Windows.Forms.TextBox metroTextBox1;
+    }
+}
+
