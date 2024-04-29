4c4688b6462db3ee817499100478a14ddba4575b
yongseok2312 <135982451+yongseok2312@users.noreply.github.com>
2024-04-30 00:11:27 +0900

    수정

---
+++ prtfolio/Miniproject_HyeinPos - v3/OhShot_Pos/FrmOrder.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace OhShot_Pos
+{
+    partial class FrmOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmOrder));
+            this.backBtn = new System.Windows.Forms.Button();
+            this.tableNum = new System.Windows.Forms.Label();
+            this.nowDateTIme = new System.Windows.Forms.Label();
+            this.totalPrice = new System.Windows.Forms.Label();
+            this.PaymentBtn = new System.Windows.Forms.Button();
+            this.AllViewBtn = new System.Windows.Forms.Button();
+            this.OrderBtn = new System.Windows.Forms.Button();
+            this.MinusBtn = new System.Windows.Forms.Button();
+            this.AllCancelBtn = new System.Windows.Forms.Button();
+            this.MenuTablePanel = new System.Windows.Forms.Panel();
+            this.BtnEggRice = new System.Windows.Forms.Button();
+            this.Btncoffee = new System.Windows.Forms.Button();
+            this.BtnSalmonRice = new System.Windows.Forms.Button();
+            this.BtnNuddle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btnminusnum = new System.Windows.Forms.Button();
+            this.MenuTablePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(28, 22);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(265, 92);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "메인으로 돌아가기";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // tableNum
+            // 
+            this.tableNum.AutoSize = true;
+            this.tableNum.Location = new System.Drawing.Point(354, 58);
+            this.tableNum.Name = "tableNum";
+            this.tableNum.Size = new System.Drawing.Size(73, 16);
+            this.tableNum.TabIndex = 1;
+            this.tableNum.Text = "tableNum";
+            // 
+            // nowDateTIme
+            // 
+            this.nowDateTIme.AutoSize = true;
+            this.nowDateTIme.Location = new System.Drawing.Point(554, 58);
+            this.nowDateTIme.Name = "nowDateTIme";
+            this.nowDateTIme.Size = new System.Drawing.Size(102, 16);
+            this.nowDateTIme.TabIndex = 1;
+            this.nowDateTIme.Text = "nowDateTIme";
+            // 
+            // totalPrice
+            // 
+            this.totalPrice.AutoSize = true;
+            this.totalPrice.Location = new System.Drawing.Point(571, 502);
+            this.totalPrice.Name = "totalPrice";
+            this.totalPrice.Size = new System.Drawing.Size(73, 16);
+            this.totalPrice.TabIndex = 1;
+            this.totalPrice.Text = "totalPrice";
+            // 
+            // PaymentBtn
+            // 
+            this.PaymentBtn.Location = new System.Drawing.Point(785, 22);
+            this.PaymentBtn.Name = "PaymentBtn";
+            this.PaymentBtn.Size = new System.Drawing.Size(265, 77);
+            this.PaymentBtn.TabIndex = 0;
+            this.PaymentBtn.Text = "결제";
+            this.PaymentBtn.UseVisualStyleBackColor = true;
+            this.PaymentBtn.Click += new System.EventHandler(this.PaymentBtn_Click);
+            // 
+            // AllViewBtn
+            // 
+            this.AllViewBtn.Location = new System.Drawing.Point(697, 120);
+            this.AllViewBtn.Name = "AllViewBtn";
+            this.AllViewBtn.Size = new System.Drawing.Size(353, 67);
+            this.AllViewBtn.TabIndex = 0;
+            this.AllViewBtn.Text = "전체보기";
+            this.AllViewBtn.UseVisualStyleBackColor = true;
+            // 
+            // OrderBtn
+            // 
+            this.OrderBtn.Location = new System.Drawing.Point(28, 470);
+            this.OrderBtn.Name = "OrderBtn";
+            this.OrderBtn.Size = new System.Drawing.Size(168, 88);
+            this.OrderBtn.TabIndex = 0;
+            this.OrderBtn.Text = "주문하기";
+            this.OrderBtn.UseVisualStyleBackColor = true;
+            this.OrderBtn.Click += new System.EventHandler(this.OrderBtn_Click);
+            // 
+            // MinusBtn
+            // 
+            this.MinusBtn.Location = new System.Drawing.Point(228, 517);
+            this.MinusBtn.Name = "MinusBtn";
+            this.MinusBtn.Size = new System.Drawing.Size(152, 41);
+            this.MinusBtn.TabIndex = 0;
+            this.MinusBtn.Text = "품목제거";
+            this.MinusBtn.UseVisualStyleBackColor = true;
+            this.MinusBtn.Click += new System.EventHandler(this.MinusBtn_Click);
+            // 
+            // AllCancelBtn
+            // 
+            this.AllCancelBtn.Location = new System.Drawing.Point(412, 470);
+            this.AllCancelBtn.Name = "AllCancelBtn";
+            this.AllCancelBtn.Size = new System.Drawing.Size(126, 90);
+            this.AllCancelBtn.TabIndex = 0;
+            this.AllCancelBtn.Text = "전체취소";
+            this.AllCancelBtn.UseVisualStyleBackColor = true;
+            this.AllCancelBtn.Click += new System.EventHandler(this.AllCancelBtn_Click);
+            // 
+            // MenuTablePanel
+            // 
+            this.MenuTablePanel.Controls.Add(this.BtnEggRice);
+            this.MenuTablePanel.Controls.Add(this.Btncoffee);
+            this.MenuTablePanel.Controls.Add(this.BtnSalmonRice);
+            this.MenuTablePanel.Controls.Add(this.BtnNuddle);
+            this.MenuTablePanel.Location = new System.Drawing.Point(697, 198);
+            this.MenuTablePanel.Name = "MenuTablePanel";
+            this.MenuTablePanel.Size = new System.Drawing.Size(353, 357);
+            this.MenuTablePanel.TabIndex = 4;
+            // 
+            // BtnEggRice
+            // 
+            this.BtnEggRice.Location = new System.Drawing.Point(28, 123);
+            this.BtnEggRice.Name = "BtnEggRice";
+            this.BtnEggRice.Size = new System.Drawing.Size(80, 29);
+            this.BtnEggRice.TabIndex = 0;
+            this.BtnEggRice.Text = "계란볶음밥";
+            this.BtnEggRice.UseVisualStyleBackColor = true;
+            this.BtnEggRice.Click += new System.EventHandler(this.BtnEggRice_Click);
+            // 
+            // Btncoffee
+            // 
+            this.Btncoffee.Location = new System.Drawing.Point(28, 88);
+            this.Btncoffee.Name = "Btncoffee";
+            this.Btncoffee.Size = new System.Drawing.Size(80, 29);
+            this.Btncoffee.TabIndex = 0;
+            this.Btncoffee.Text = "커피";
+            this.Btncoffee.UseVisualStyleBackColor = true;
+            this.Btncoffee.Click += new System.EventHandler(this.Btncoffee_Click);
+            // 
+            // BtnSalmonRice
+            // 
+            this.BtnSalmonRice.Location = new System.Drawing.Point(28, 53);
+            this.BtnSalmonRice.Name = "BtnSalmonRice";
+            this.BtnSalmonRice.Size = new System.Drawing.Size(80, 29);
+            this.BtnSalmonRice.TabIndex = 0;
+            this.BtnSalmonRice.Text = "연어덮밥";
+            this.BtnSalmonRice.UseVisualStyleBackColor = true;
+            this.BtnSalmonRice.Click += new System.EventHandler(this.BtnSalmonRice_Click);
+            // 
+            // BtnNuddle
+            // 
+            this.BtnNuddle.Location = new System.Drawing.Point(28, 18);
+            this.BtnNuddle.Name = "BtnNuddle";
+            this.BtnNuddle.Size = new System.Drawing.Size(80, 29);
+            this.BtnNuddle.TabIndex = 0;
+            this.BtnNuddle.Text = "라면";
+            this.BtnNuddle.UseVisualStyleBackColor = true;
+            this.BtnNuddle.Click += new System.EventHandler(this.BtnNuddle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(654, 339);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(555, 524);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 1);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(654, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "원";
+            // 
+            // Btnminusnum
+            // 
+            this.Btnminusnum.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btnminusnum.Location = new System.Drawing.Point(228, 470);
+            this.Btnminusnum.Name = "Btnminusnum";
+            this.Btnminusnum.Size = new System.Drawing.Size(152, 41);
+            this.Btnminusnum.TabIndex = 0;
+            this.Btnminusnum.Text = "수량감소";
+            this.Btnminusnum.UseVisualStyleBackColor = true;
+            this.Btnminusnum.Click += new System.EventHandler(this.Btnminusnum_Click);
+            // 
+            // FrmOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 572);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.MenuTablePanel);
+            this.Controls.Add(this.totalPrice);
+            this.Controls.Add(this.nowDateTIme);
+            this.Controls.Add(this.tableNum);
+            this.Controls.Add(this.AllViewBtn);
+            this.Controls.Add(this.PaymentBtn);
+            this.Controls.Add(this.AllCancelBtn);
+            this.Controls.Add(this.Btnminusnum);
+            this.Controls.Add(this.MinusBtn);
+            this.Controls.Add(this.OrderBtn);
+            this.Controls.Add(this.backBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmOrder";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmOrder_Load);
+            this.MenuTablePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Label tableNum;
+        private System.Windows.Forms.Label nowDateTIme;
+        private System.Windows.Forms.Label totalPrice;
+        private System.Windows.Forms.Button PaymentBtn;
+        private System.Windows.Forms.Button AllViewBtn;
+        private System.Windows.Forms.Button OrderBtn;
+        private System.Windows.Forms.Button MinusBtn;
+        private System.Windows.Forms.Button AllCancelBtn;
+        private System.Windows.Forms.Panel MenuTablePanel;
+        private System.Windows.Forms.Button BtnEggRice;
+        private System.Windows.Forms.Button Btncoffee;
+        private System.Windows.Forms.Button BtnSalmonRice;
+        private System.Windows.Forms.Button BtnNuddle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btnminusnum;
+    }
+}
---
+++ prtfolio/Miniproject_HyeinPos - v3/OhShot_Pos/FrmOrder.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OhShot_Pos
+{
+    public partial class FrmOrder : Form
+    {
+        string menuName = string.Empty;
+        private SqlConnection conn = null;
+        private SqlTransaction transaction = null;
+
+        public FrmOrder()
+        {
+            InitializeComponent();
+            TransactionStart();
+        }
+
+        private void FrmOrder_Load(object sender, EventArgs e)
+        {
+            tableNum.Text = string.Empty;
+            tableNum.Text = "Table 1";
+            nowDateTIme.Text = DateTime.Now.ToString("T");
+            totalPrice.Text = string.Empty;
+            
+            RefreshData();
+        }
+                        
+        private void RefreshData()
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"SELECT 
+                                      [menuName]
+                                      ,[menuPrice]
+                                      ,[menunum]
+                                  FROM [table1]";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "table1");
+                    dataGridView1.DataSource = ds.Tables["table1"];
+                    dataGridView1.Columns[0].HeaderText = "메뉴";
+                    dataGridView1.Columns[1].HeaderText = "가격";
+                    dataGridView1.Columns[2].HeaderText = "수량";
+
+                    
+                    decimal totalMenuPrice = sumprice(conn);
+                    totalPrice.Text = totalMenuPrice.ToString("C");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"데이터 가져오기 오류: {ex.Message}");
+                }
+            }
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            RollbackTransaction();
+            this.Dispose();
+        }
+
+        private void PaymentBtn_Click(object sender, EventArgs e)
+        {
+            CommitTransaction();
+            this.Dispose();
+        }
+
+
+        #region '주문하기' 버튼 클릭 이벤트 핸들러 
+        private void OrderBtn_Click(object sender, EventArgs e)
+        {
+            CommitTransaction();
+            MessageBox.Show("주문이 완료되었습니다.");
+            this.Dispose();
+        }
+        #endregion
+
+
+        #region '품목제거' 버튼 클릭 이벤트 핸들러 
+        private void MinusBtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                string menuName = selectedRow.Cells["menuName"].Value.ToString(); // 메뉴 이름 가져오기
+                DeleteMenu(menuName);
+                RefreshData();
+            }
+            else
+            {
+                MessageBox.Show("삭제할 품목을 선택해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        #endregion
+
+        #region '단품취소' 버튼 클릭 이벤트 핸들러
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region '전체취소' 버튼 클릭 이벤트 핸들러
+        private void AllCancelBtn_Click(object sender, EventArgs e)
+        {
+            Alldelete();
+            RefreshData();
+        }
+
+
+        #endregion
+
+
+        private bool CheckIfMenuExists(SqlConnection conn, string menuName) // 동일한 메뉴이름이 확인
+        {
+            string query = "SELECT COUNT(*) FROM [table1] WHERE [menuName] = @menuName";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@menuName", menuName);
+
+            int count = (int)cmd.ExecuteScalar();
+            return count > 0;
+        }
+        private void IncreaseMenuQuantity(SqlConnection conn, string menuName) // 수량 증가
+        {
+            string query = "UPDATE [table1] SET [menunum] = [menunum] + 1 WHERE [menuName] = @menuName";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@menuName", menuName);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        private void AddNewMenu(SqlConnection conn, string menuName, int menuPrice, int menuQuantity) // 메뉴 추가
+        {
+            string query = @"INSERT INTO [table1] ([menuName], [menuPrice], [menunum]) VALUES (@menuName, @menuPrice, @menuQuantity)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@menuName", menuName);
+            cmd.Parameters.AddWithValue("@menuPrice", menuPrice);
+            cmd.Parameters.AddWithValue("@menuQuantity", menuQuantity);
+
+            cmd.ExecuteNonQuery();
+        }
+        #region "메뉴"
+        private void BtnSalmonRice_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                bool isRamenExists = CheckIfMenuExists(conn, "연어덮밥");
+                if (isRamenExists)
+                {
+                    // '라면' 메뉴가 이미 테이블에 존재하면 수량을 증가시킴
+                    IncreaseMenuQuantity(conn, "연어덮밥");
+                }
+                else
+                {
+                    // '라면' 메뉴가 테이블에 없으면 새로 추가
+                    AddNewMenu(conn, "연어덮밥", 15000, 1); // 메뉴명, 가격, 수량
+                }
+                RefreshData();
+
+            }
+        }
+        private void BtnNuddle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                bool isRamenExists = CheckIfMenuExists(conn, "라면");
+                if (isRamenExists)
+                {
+                    // '라면' 메뉴가 이미 테이블에 존재하면 수량을 증가시킴
+                    IncreaseMenuQuantity(conn, "라면");
+                }
+                else
+                {
+                    // '라면' 메뉴가 테이블에 없으면 새로 추가
+                    AddNewMenu(conn, "라면", 2000, 1); // 메뉴명, 가격, 수량
+                }
+                RefreshData();
+
+            }
+        }
+
+        private void Btncoffee_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                bool isRamenExists = CheckIfMenuExists(conn, "커피");
+                if (isRamenExists)
+                {
+                    // '라면' 메뉴가 이미 테이블에 존재하면 수량을 증가시킴
+                    IncreaseMenuQuantity(conn, "커피");
+                }
+                else
+                {
+                    // '라면' 메뉴가 테이블에 없으면 새로 추가
+                    AddNewMenu(conn, "커피", 3000, 1); // 메뉴명, 가격, 수량
+                }
+                RefreshData();
+
+            }
+        }
+
+        private void BtnEggRice_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                bool isRamenExists = CheckIfMenuExists(conn, "계란볶음밥");
+                if (isRamenExists)
+                {
+                    // '라면' 메뉴가 이미 테이블에 존재하면 수량을 증가시킴
+                    IncreaseMenuQuantity(conn, "계란볶음밥");
+                }
+                else
+                {
+                    // '라면' 메뉴가 테이블에 없으면 새로 추가
+                    AddNewMenu(conn, "계란볶음밥", 6500, 1); // 메뉴명, 가격, 수량
+                }
+                RefreshData();
+
+            }
+
+            
+        }
+        #endregion
+
+        private int sumprice(SqlConnection conn)
+        {
+            int totalMenuPrice = 0;
+            string query = @"SELECT SUM([menuPrice]*[menunum]) AS TotalMenuPrice FROM table1";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            object result = cmd.ExecuteScalar();
+            if (result != null && int.TryParse(result.ToString(), out totalMenuPrice))
+            {
+                return totalMenuPrice;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        private void Alldelete()
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"DELETE
+                                  FROM [table1]";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "table1");
+                    dataGridView1.DataSource = ds.Tables["table1"];
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"데이터 가져오기 오류: {ex.Message}");
+                }
+            }
+        }
+        
+        private void DeleteMenu(string menuName)
+        {
+            using(SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                string query = @"Delete FROM [table1] WHERE [menuName] = @menuName ";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@menuName", menuName);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        private void MinuNum(string menuName)
+        {
+            using (SqlConnection conn = new SqlConnection(Helper.Common.connString))
+            {
+                conn.Open();
+                string query = "UPDATE [table1] SET [menunum] = [menunum] - 1 WHERE [menuName] = @menuName";
+
+                // SqlCommand 객체 생성 시 SqlConnection 객체 전달
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@menuName", menuName);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void Btnminusnum_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                string menuName = selectedRow.Cells["menuName"].Value.ToString(); // 메뉴 이름 가져오기
+                string menuNum = selectedRow.Cells["menunum"].Value.ToString(); // 메뉴 이름 가져오기
+                MinuNum(menuName);
+                if(menuNum == "1") { DeleteMenu(menuName);}
+                RefreshData();
+            }
+            else
+            {
+                MessageBox.Show("삭제할 품목을 선택해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void TransactionStart()
+        {
+            conn = new SqlConnection(Helper.Common.connString);
+            conn.Open();
+            transaction = conn.BeginTransaction();
+        }
+
+        private void CommitTransaction()
+        {
+            transaction.Commit();
+            conn.Close();
+        }
+        private void RollbackTransaction()
+        {
+            transaction.Rollback();
+            conn.Close();
+        }
+    }
+}
+
+
+
